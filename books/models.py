9a4f02fae57b2ca07d9b3300c995afdb9eb6c487
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-27 16:27:38 +0200

    feat: add Book model with fields and migrations

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+
+    class Cover(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.IntegerField(choices=Cover.choices, default=None)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
