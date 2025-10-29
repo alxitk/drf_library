41204d7a0f6bcffe0bcf6b0638f5407179eac1b9
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-29 13:08:01 +0200

    feat(users): add admin users list endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from users.views import CreateUserView, ManageUserView
+from users.views import CreateUserView, ManageUserView, UserViewSet
 
 app_name = "users"
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
@@ -11,4 +15,4 @@ urlpatterns = [
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage_user")
-]
+] + router.urls
