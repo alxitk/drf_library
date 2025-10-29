e12cb2c08a0f7206a071a34d327146ffff821fb4
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-29 12:48:52 +0200

    feat(api): add Swagger documentation and implement user viewsets
    - Integrated drf-spectacular for automatic Swagger documentation
    - Configured DEFAULT_SCHEMA_CLASS in settings.py
    - Implemented UserViewSet and related serializers
    - Added API routes for user endpoints
    - Updated project URLs to include schema and Swagger UI

--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer, AuthTokenSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = (IsAdminUser,)
+
+
+class CreateUserView(generics.CreateAPIView):
+   serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+   serializer_class = AuthTokenSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+   serializer_class = UserSerializer
+   permission_classes = (IsAuthenticated,)
+
+   def get_object(self):
+      return self.request.user
