50b46b692b359389d2cc393c7845470a5ef9232a
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-30 17:12:25 +0200

    feat(borrowings): add __str__ method to Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -5,7 +5,7 @@ from books.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
+    borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
@@ -14,3 +14,6 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowings"
     )
+
+    def __str__(self):
+        return f"{self.book} - {self.user}"
