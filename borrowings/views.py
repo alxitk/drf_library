8cd3725c6aed8baeb61099998a59ceefa442d91c
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-30 23:43:47 +0200

    feat(borrowings): restrict queryset to user and require authentication

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from borrowings.models import Borrowing
@@ -6,9 +7,15 @@ from borrowings.serializers import BorrowingListSerializer, BorrowingSerializer
 
 class BorrowingsListView(ModelViewSet):
     queryset = Borrowing.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.action == "list":
             return BorrowingListSerializer
         else:
             return BorrowingSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Borrowing.objects.all()
+        return Borrowing.objects.filter(user=self.request.user)
