f38557d270beb7115715786508096059a47a5e9c
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-31 23:29:30 +0200

    test(borrowings): add tests for return_borrowing

--- borrowings/tests.py
+++ borrowings/tests.py
@@ -7,7 +7,7 @@ from rest_framework.test import APIClient
 from books.models import Book
 from borrowings.models import Borrowing
 
-BORROWING_URL = reverse("borrowings:borrowing-list")
+BORROWING_URL = reverse("borrowings:borrowings-list")
 
 class UnauthenticatedBorrowingApiTests(TestCase):
     def setUp(self):
@@ -54,4 +54,43 @@ class AuthenticatedBorrowingApiTests(TestCase):
         response = self.client.get(BORROWING_URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
+
+
+class ReturnBorrowingApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user1 = get_user_model().objects.create_user(
+            email="<EMAIL>",
+            password="<PASSWORD>",
+        )
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="Test Author",
+            cover="Hard",
+            inventory=1,
+            daily_fee=10.10,
+        )
+        self.borrowing = Borrowing.objects.create(
+            user = self.user1,
+            book = self.book,
+            borrow_date = "2025-10-01",
+            expected_return_date = "2025-10-10",
+        )
+        self.client.force_authenticate(user=self.user1)
+
+    def test_return_borrowing_success(self):
+        url = reverse("borrowings:borrowings-return-borrowing", args=[self.borrowing.id])
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.borrowing.refresh_from_db()
+        self.assertIsNotNone(self.borrowing.actual_return_date)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.inventory, 2)
+
+    def test_cannot_return_twice(self):
+        url = reverse("borrowings:borrowings-return-borrowing", args=[self.borrowing.id])
+        self.client.post(url)
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
 
